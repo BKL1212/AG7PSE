2d190e567a80ea7ea927cbc70e054de9cd02a58d
BKL1212 <46940950+BKL1212@users.noreply.github.com>
2019-01-22 21:58:49 +0100

    Add files via upload

---
+++ templates/footer.inc.php
@@ -0,0 +1,16 @@
+<?php 
+?>
+ <hr>
+  <div class="container">
+  	<footer>
+       
+      </footer>
+   </div> 
+<!--So sieht der  footer aus-->
+
+   
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery.min.js"><\/script>')</script>
+    <script src="js/bootstrap.min.js"></script>
+  </body>
+</html>
